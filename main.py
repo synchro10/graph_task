ca9e0f935d6f94c159f0a2307a418978ae697880
a.kondratev <a.kondratev@expasoft.ru>
2018-10-12 15:17:01 +0700

    solver interface, clean main

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-import networkx as nx
-import numpy as np
+from graph import NxGraph
+from task_solver import SimpleSolver
 
-graph = nx.Graph()
-graph.add_node("v1")
-graph.add_node("v2")
-graph.add_edge("v1", "v2")
-graph.node["v1"]["weight"] = 3.0
-graph.node["v2"]["weight"] = 2.0
-print(list(graph.nodes(data=True)))
-print(list(graph.edges()))
-
-# print(graph)
-# for g in graph:
-#     print(g, )
+graph = NxGraph()
+graph.init_from_txt("res/test1.txt")
+solver_kwargs = {
+    "graph": graph,
+}
+solver = SimpleSolver(**solver_kwargs)
+answer = solver.get_answer()
---
+++ task_solver.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from overrides import overrides
+
+import graph as graph
+
+class TaskSolver(ABC):
+
+    _graph = None
+
+    def __init__(self, **kwargs):
+        self._graph = kwargs.pop("graph")
+
+    @abstractmethod
+    def get_answer(self):
+        pass
+
+
+class SimpleSolver(TaskSolver):
+
+    @overrides
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    @overrides
+    def get_answer(self):
+        pass
