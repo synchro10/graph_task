36e509896f2dc85b1eab62b836eb41d546da07e2
a.kondratev <a.kondratev@expasoft.ru>
2018-10-13 16:42:40 +0700

    clean up main

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from graph import NxGraph
 from task_solver import SimpleSolver
 
-graph = NxGraph()
-graph.init_from_txt("res/test1.txt")
-graph.print_graph()
-solver_kwargs = {
-    "graph": graph,
-}
-solver = SimpleSolver(**solver_kwargs)
-answer = solver.get_answer()
+
+def example():
+    test = "res/test1.txt"
+    graph = NxGraph()
+    graph.init_from_txt(test)
+    print("Graph before")
+    graph.print_graph()
+    solver_kwargs = {
+        "graph": graph,
+    }
+    solver = SimpleSolver(**solver_kwargs)
+    answer = solver.get_answer()
+    answer.print()
+    if graph.interpretate_task_answer(answer):
+        print(test, "Passed")
+    else:
+        print(test, "Failed")
+    print("Graph after")
+    graph.print_graph(only_nodes=True)
+
+
+example()
